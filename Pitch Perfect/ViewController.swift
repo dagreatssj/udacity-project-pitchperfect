cb793d1c3fb85b19503f93b3bc1debe6ad5a888e
Darrell Calderon <dagreatssj@gmail.com>
2014-11-24 18:12:36 -0800

    added a navigation controller to go to another screen

--- Pitch Perfect/ViewController.swift
+++ Pitch Perfect/ViewController.swift
@@ -9,10 +9,10 @@
 import UIKit
 
 class ViewController: UIViewController {
-
-    // created a weak variable
     @IBOutlet weak var recordingInProgress: UILabel!
-    
+    @IBOutlet weak var stopButton: UIButton!
+    @IBOutlet weak var recordButton: UIButton!
+
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -22,20 +22,35 @@ class ViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+    override func viewWillAppear(animated: Bool) {
+        // hide the stop button
+        stopButton.hidden = true
+        // enable the record button
+        recordButton.enabled = true
+    }
 
     // action method for the UIButton (microphone) to start recording
     @IBAction func recordAudio(sender: UIButton) {
+        // show the stop button when begin recording
+        stopButton.hidden = false
+
         // when image is pressed show the recording text
         recordingInProgress.hidden = false
         
+        // make sure user cannot hit the microphone button twice
+        recordButton.enabled = false
+        
         // TODO: Record the user's voice
-        println("in recordAudio")
     }
     
     // action method for the UIButton (stop) to stop recording
     @IBAction func stopRecord(sender: UIButton) {
+        // hide the recording label
         recordingInProgress.hidden = true
-        println("stop recording")
+        
+        // hide the stop button
+        stopButton.hidden = true
     }
 }
 
